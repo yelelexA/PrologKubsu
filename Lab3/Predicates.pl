1d51bda1c6534884aeb57525df9b287e17090480
yelelexA <axxxeleley@gmail.com>
2021-03-05 23:06:49 +0300

    Task3
    Complete task #3.

--- Lab3/Predicates.pl
+++ Lab3/Predicates.pl
@@ -6,3 +6,7 @@ max(_, Y, Z) :- Z is Y.
 max(X, Y, U, Z) :- X >= Y, X >= U, Z is X, !.
 max(_, Y, U, Z) :- Y >= U, Z is Y, !.
 max(_, _, U, Z) :- Z is U.
+
+% Task #3
+fact(0, 1) :- !.
+fact(N, X) :- N1 is N - 1, fact(N1, X1), X is X1 * N.
