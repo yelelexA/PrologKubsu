9c9a1fe0b829b8a689f4e65328f0595b410e4fd5
yelelexA <axxxeleley@gmail.com>
2021-03-05 23:12:28 +0300

    Task4
    Complete task #4.

--- Lab3/Predicates.pl
+++ Lab3/Predicates.pl
@@ -10,3 +10,8 @@ max(_, _, U, Z) :- Z is U.
 % Task #3
 fact(0, 1) :- !.
 fact(N, X) :- N1 is N - 1, fact(N1, X1), X is X1 * N.
+
+% Task #4
+fact(0, CurX, CurX) :- !.
+fact(N, CurX, X) :- CurX1 is CurX * N, N1 is N - 1, fact(N1, CurX1, X).
+factN(N, X) :- fact(N, 1, X).
