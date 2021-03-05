dfaa029e67db2f81a8ff0c503004545cb0511be1
yelelexA <axxxeleley@gmail.com>
2021-03-05 22:58:16 +0300

    Task2
    Complete task #2.

--- Lab3/Predicates.pl
+++ Lab3/Predicates.pl
@@ -1,3 +1,8 @@
 % Task #1
 max(X, Y, Z) :- X >= Y, Z is X, !.
 max(_, Y, Z) :- Z is Y.
+
+% Task #2
+max(X, Y, U, Z) :- X >= Y, X >= U, Z is X, !.
+max(_, Y, U, Z) :- Y >= U, Z is Y, !.
+max(_, _, U, Z) :- Z is U.
