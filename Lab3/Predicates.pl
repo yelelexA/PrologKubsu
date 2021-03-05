5f566b812dd26e200d1d7e1a4b8e094addf89571
yelelexA <axxxeleley@gmail.com>
2021-03-05 23:24:59 +0300

    Task6
    Complete task #6.

--- Lab3/Predicates.pl
+++ Lab3/Predicates.pl
@@ -20,3 +20,9 @@ factN(N, X) :- fact(N, 1, X).
 fib(1, 1) :- !.
 fib(2, 1) :- !.
 fib(N, X) :- N1 is N - 1, N2 is N - 2, fib(N1, X1), fib(N2, X2), X is X1 + X2.
+
+% Task #6
+fib(1, _, CurX2, CurX2) :- !.
+fib(2, _, CurX2, CurX2) :- !.
+fib(N, CurX1, CurX2, X) :- CurX3 is CurX1 + CurX2, N1 is N - 1, fib(N1, CurX2, CurX3, X).
+fibN(N, X) :- fib(N, 1, 1, X).
