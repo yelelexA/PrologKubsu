159ee602360d8b7ec7a5d000bdacff4659758240
yelelexA <axxxeleley@gmail.com>
2021-03-06 00:44:17 +0300

    40objects
    Added "40objects.pl".

---
+++ Lab2/40objects.pl
@@ -0,0 +1,306 @@
+date(max_payne, 0).
+date(serious_sam, 0).
+date(claude, 0).
+date(thomas_angelo, 0).
+date(bastila_shan, 0).
+date(sam_fisher, 0).
+date(jack_carver, 1).
+date(rico_rodriguez, 1).
+date(cj, 1).
+date(kratos, 1).
+date(alex_mercer, 2).
+date(captain_price, 2).
+date(big_daddy, 2).
+date(geralt, 2).
+date(captain_shepherd, 2).
+date(altair, 2).
+date(ezio, 2).
+date(stiv, 2).
+date(niko_belik, 2).
+date(john_marston, 3).
+date(isaac, 3).
+date(batman, 3).
+date(corvo_attano, 3).
+date(vaas_montenegro, 3).
+date(joel, 4).
+date(ellie, 4).
+date(lee_everett, 4).
+date(clementine, 4).
+date(nathan_drake, 4).
+date(aiden_pearce, 4).
+date(michael_de_santa, 4).
+date(trevor_philips, 4).
+date(franklin_clinton, 4).
+date(tracer, 5).
+date(eloy, 5).
+date(connor, 6).
+date(sam, 6).
+date(deacon_st_john, 6).
+date(abby, 6).
+date(miles_morales, 6).
+
+time(max_payne, 1).
+time(serious_sam, 5).
+time(claude, 1).
+time(thomas_angelo, 1).
+time(bastila_shan, 5).
+time(sam_fisher, 2).
+time(jack_carver, 2).
+time(rico_rodriguez, 2).
+time(cj, 2).
+time(kratos, 2).
+time(alex_mercer, 5).
+time(captain_price, 3).
+time(big_daddy, 4).
+time(geralt, 0).
+time(captain_shepherd, 4).
+time(altair, 0).
+time(ezio, 0).
+time(stiv, 5).
+time(niko_belik, 1).
+time(john_marston, 0).
+time(isaac, 5).
+time(batman, 3).
+time(corvo_attano, 5).
+time(vaas_montenegro, 2).
+time(joel, 3).
+time(ellie, 3).
+time(lee_everett, 3).
+time(clementine, 3).
+time(nathan_drake, 2).
+time(aiden_pearce, 3).
+time(michael_de_santa, 2).
+time(trevor_philips, 2).
+time(franklin_clinton, 2).
+time(tracer, 3).
+time(eloy, 5).
+time(connor, 4).
+time(sam, 4).
+time(deacon_st_john, 4).
+time(abby, 3).
+time(miles_morales, 2).
+
+age(max_payne, 3).
+age(serious_sam, 3).
+age(claude, 2).
+age(thomas_angelo, 3).
+age(bastila_shan, 2).
+age(sam_fisher, 3).
+age(jack_carver, 3).
+age(rico_rodriguez, 3).
+age(cj, 2).
+age(kratos, 4).
+age(alex_mercer, 2).
+age(captain_price, 3).
+age(big_daddy, 4).
+age(geralt, 3).
+age(captain_shepherd, 2).
+age(altair, 3).
+age(ezio, 2).
+age(stiv, 2).
+age(niko_belik, 3).
+age(john_marston, 0).
+age(isaac, 0).
+age(batman, 3).
+age(corvo_attano, 3).
+age(vaas_montenegro, 3).
+age(joel, 3).
+age(ellie, 1).
+age(lee_everett, 3).
+age(clementine, 0).
+age(nathan_drake, 3).
+age(aiden_pearce, 3).
+age(michael_de_santa, 3).
+age(trevor_philips, 3).
+age(franklin_clinton, 2).
+age(tracer, 2).
+age(eloy, 2).
+age(connor, 2).
+age(sam, 3).
+age(deacon_st_john, 2).
+age(abby, 2).
+age(miles_morales, 1).
+
+death(max_payne, 0).
+death(serious_sam, 0).
+death(claude, 0).
+death(thomas_angelo, 2).
+death(bastila_shan, 1).
+death(sam_fisher, 0).
+death(jack_carver, 2).
+death(rico_rodriguez, 0).
+death(cj, 0).
+death(kratos, 0).
+death(alex_mercer, 2).
+death(captain_price, 0).
+death(big_daddy, 2).
+death(geralt, 0).
+death(captain_shepherd, 0).
+death(altair, 1).
+death(ezio, 1).
+death(stiv, 0).
+death(niko_belik, 0).
+death(john_marston, 2).
+death(isaac, 0).
+death(batman, 0).
+death(corvo_attano, 2).
+death(vaas_montenegro, 2).
+death(joel, 2).
+death(ellie, 0).
+death(lee_everett, 3).
+death(clementine, 0).
+death(nathan_drake, 0). 
+death(aiden_pearce, 0).
+death(michael_de_santa, 3).
+death(trevor_philips, 3).
+death(franklin_clinton, 0).
+death(tracer, 0).
+death(eloy, 0).
+death(connor, 3).
+death(sam, 0).
+death(deacon_st_john, 0).
+death(abby, 0).
+death(miles_morales, 0).
+
+type(max_payne, 0).
+type(serious_sam, 0).
+type(claude, 2).
+type(thomas_angelo, 2).
+type(bastila_shan, 0).
+type(sam_fisher, 0).
+type(jack_carver, 2).
+type(rico_rodriguez, 0).
+type(cj, 2).
+type(kratos, 0).
+type(alex_mercer, 1).
+type(captain_price, 0).
+type(big_daddy, 1).
+type(geralt, 0).
+type(captain_shepherd, 0).
+type(altair, 0).
+type(ezio, 0).
+type(stiv, 0).
+type(niko_belik, 2).
+type(john_marston, 2).
+type(isaac, 0).
+type(batman, 0).
+type(corvo_attano, 0).
+type(vaas_montenegro, 1).
+type(joel, 0).
+type(ellie, 0).
+type(lee_everett, 0).
+type(clementine, 0).
+type(nathan_drake, 0).
+type(aiden_pearce, 0).
+type(michael_de_santa, 0).
+type(trevor_philips, 2).
+type(franklin_clinton, 0).
+type(tracer, 0).
+type(eloy, 0).
+type(connor, 0).
+type(sam, 0).
+type(deacon_st_john, 0).
+type(abby, 2).
+type(miles_morales, 0).
+
+gender(max_payne, 0).
+gender(serious_sam, 0).
+gender(claude, 0).
+gender(thomas_angelo, 0).
+gender(bastila_shan, 1).
+gender(sam_fisher, 0).
+gender(jack_carver, 0).
+gender(rico_rodriguez, 0).
+gender(cj, 0).
+gender(kratos, 0).
+gender(alex_mercer, 0).
+gender(captain_price, 0).
+gender(big_daddy, 0).
+gender(geralt, 0).
+gender(captain_shepherd, 0).
+gender(altair, 0).
+gender(ezio, 0).
+gender(stiv, 0).
+gender(niko_belik, 0).
+gender(john_marston, 0).
+gender(isaac, 0).
+gender(batman, 0).
+gender(corvo_attano, 0).
+gender(vaas_montenegro, 0).
+gender(joel, 0).
+gender(ellie, 1).
+gender(lee_everett, 0).
+gender(clementine, 1).
+gender(nathan_drake, 0). 
+gender(aiden_pearce, 0).
+gender(michael_de_santa, 0).
+gender(trevor_philips, 0).
+gender(franklin_clinton, 0).
+gender(tracer, 1).
+gender(eloy, 1).
+gender(connor, 0).
+gender(sam, 0).
+gender(deacon_st_john, 0).
+gender(abby, 1).
+gender(miles_morales, 0).
+
+question1(X1) :- write("In what year did the game with this character first come out?"), nl,
+				 write("0. 2001 - 2003"), nl,
+				 write("1. 2004 - 2006"), nl,
+				 write("2. 2007 - 2009"), nl,
+				 write("3. 2010 - 2012"), nl,
+				 write("4. 2013 - 2015"), nl,
+				 write("5. 2016 - 2018"), nl,
+				 write("6. 2018 - 2020"), nl,
+				 read(X1).
+
+question2(X2) :- write("What time does the character live in?"), nl,
+				 write("0. The distant past "), nl,
+				 write("1. Recent past"), nl,
+				 write("2. The present"), nl,
+				 write("3. The near future"), nl,
+				 write("4. The distant future"), nl,
+				 write("5. Unknown fantasy world"), nl,
+				 read(X2).
+
+
+question3(X3) :- write("What age group does the character belong to?"), nl,
+				 write("0. Kid (12-)"), nl,
+				 write("1. Teenager (13-21)"), nl,
+    			 write("2. Young (22-30)"), nl,
+    			 write("3. Adult (31-55)"), nl,
+				 write("4. Elderly (56+)"), nl,
+				 read(X3).
+
+question4(X4) :- write("How did your character die?"), nl,
+				 write("0. Alive"), nl,
+				 write("1. Died of old age"), nl,
+    			 write("2. Was killed"), nl,
+				 write("3. Depends on the choice"), nl,
+				 read(X4).
+
+question5(X5) :- write("Is your character a type or a villain?"), nl,
+				 write("0. Hero"), nl,
+				 write("1. Villian"), nl,
+    			 write("2. Controversial"), nl,
+				 read(X5).
+
+question6(X6) :- write("What gender is your character?"), nl,
+				 write("0. Male"), nl,
+				 write("1. Female"), nl,
+				 read(X6).
+
+pr :-
+  question1(X1),
+  question2(X2),
+  question3(X3),
+  question4(X4),
+  question5(X5),
+  question6(X6),
+  date(X, X1),
+  time(X, X2),
+  age(X, X3),
+  death(X, X4),
+  type(X, X5),
+  gender(X, X6),
+  write(X).
